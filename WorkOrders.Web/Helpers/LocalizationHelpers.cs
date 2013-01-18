ef9b1d0b1e98830f17bfd67242e7176a8c3582fe
Goran Obradović <gazda@goranobradovic.com>
2013-01-18 08:14:00 +0100

    refactoring - localization, editor templates

---
+++ WorkOrders.Web/Helpers/LocalizationHelpers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Resources;
+
+namespace WorkOrders.Web.Helpers
+{
+    public static class LocalizationHelpers
+    {
+        public static string DateFormat(this HtmlHelper helper)
+        {
+            if (string.IsNullOrEmpty(Localize.DateFormat))
+            {
+                return CultureInfo.CurrentUICulture.DateTimeFormat.ShortDatePattern;
+            }
+            return Localize.DateFormat;
+        }
+    }
+}
---
+++ WorkOrders.Web/Models/WorkOrdersViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WorkOrders.Domain.Models;
+
+namespace WorkOrders.Web.Models
+{
+    public class WorkOrdersViewModel
+    {
+        public ICollection<WorkOrder> WorkOrdersList { get; set; }
+
+        public WorkOrder SelectedWokOrder { get; set; }
+    }
+}
